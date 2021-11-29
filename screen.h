0fc952f7591d99dfad6707135eacc3652a8d1fb5
Николаева Валерия Леонидовна <v.nikolaeva.2018@stud.nstu.ru>
2021-11-29 23:47:55 +0700

    Created pattern

---
+++ screen.h
@@ -0,0 +1,20 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include <QString>
+#include <iostream>
+
+using namespace std;
+
+class Screen
+{
+private:
+    QString description;
+public:
+    Screen(QString description);
+    void up();
+    void down();
+    QString toString();
+};
+
+#endif // SCREEN_H
